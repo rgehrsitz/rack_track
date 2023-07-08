5ab86557c67707aa4cda254ca4555da30f19abd4
robertg <36572841+rgehrsitz@users.noreply.github.com>
2023-07-07 20:51:20 -0400

    Adding JSON handling

--- lib/equipment.dart
+++ lib/equipment.dart
@@ -14,4 +14,30 @@ class Equipment {
     required this.properties,
     required this.children,
   });
+
+  // Convert an Equipment object to a JSON map.
+  Map<String, dynamic> toJson() {
+    return {
+      'id': id,
+      'name': name,
+      'createdAt': createdAt.toIso8601String(),
+      'updatedAt': updatedAt.toIso8601String(),
+      'properties': properties,
+      'children': children.map((e) => e.toJson()).toList(),
+    };
+  }
+
+  // Create an Equipment object from a JSON map.
+  static Equipment fromJson(Map<String, dynamic> json) {
+    return Equipment(
+      id: json['id'],
+      name: json['name'],
+      createdAt: DateTime.parse(json['createdAt']),
+      updatedAt: DateTime.parse(json['updatedAt']),
+      properties: Map<String, dynamic>.from(json['properties']),
+      children: (json['children'] as List)
+          .map((e) => Equipment.fromJson(e as Map<String, dynamic>))
+          .toList(),
+    );
+  }
 }
